6a4b00abfcc0628217653c1a76207ac86351b47d
crismane <crismane@sas.upenn.edu>
2015-04-26 03:03:14 -0400

    Update RandomWalkerSeg.m

--- RandomWalkerSeg.m
+++ RandomWalkerSeg.m
@@ -1,21 +1,21 @@
-function [mask] = RandomWalkerSeg(datavector,Beta)
+function [mask,P] = RandomWalkerSeg(datavector,Beta)
 %Random Walker Algorithm for Segmentation Challenge
 %Image is the image as a matrix of intensities
 %Beta is a free parameter used in the weighting function
 
 %% Define the Lattice Structure based on the size of the Image
 image=datavector.img;
-seed=datavector.seeds;
+seed=datavector.seed;
 
 
 % Obtain the dimensions of the image
-[xdim,ydim]=size(image);
+[xdim,ydim,o]=size(image);
 
 % Create a grid of those dimensions starting at 0
-xrange=0:(xdim-1);
-yrange=0:(ydim-1);
-[xgrid, ygrid]=meshgrid(xrange,yrange);
-gridpoints=[xgrid(:),ygrid(:)];
+%xrange=0:(xdim-1);
+%yrange=0:(ydim-1);
+%[xgrid, ygrid]=meshgrid(xrange,yrange);
+%gridpoints=[xgrid(:),ygrid(:)];
 Npoints=xdim*ydim;
 
 % Define the edges connecting the points
@@ -41,6 +41,10 @@ eps=10^-6; %Based on paper
 %Define the weight function as described in the paper
 wij=exp(Beta/rho*(Idiff))+eps;
 
+%clear Beta 
+clear rho
+clear Idiff
+clear eps
 %% Calculate the sparse Laplacian matrix L with elements Lm, B, B', and Lu
 
 % Calculate a sparse 2D matrix of the weights, which will place weights at
@@ -48,6 +52,10 @@ wij=exp(Beta/rho*(Idiff))+eps;
 % (non-adjacent nodes based on the formulation for Lij) for optimization
 % purposes
 sparseweights=sparse([edges(:,1);edges(:,2)],[edges(:,2);edges(:,1)],[wij;wij],Npoints,Npoints);
+
+clear edges 
+clear Npoints
+clear wij
 
 % Calculate L with Lij=di (sum of the wij terms along i minus wij) for i=j and -wij for adjacent points
 % The diag function in Matlab assigns values to i=j, so by assigning the
@@ -57,32 +65,52 @@ sparseweights=sparse([edges(:,1);edges(:,2)],[edges(:,2);edges(:,1)],[wij;wij],N
 L=diag(sum(sparseweights));
 L=L-sparseweights;
 
+clear sparseweights
+
 %% Assign labels from the seed
 
 % Find the number of seeds, which equals the number of labels
 sparseseed=sparse(seed);
 nseeds=nnz(sparseseed);
-nlabels=nseeds;
+%nlabels=nseeds;
+labels=unique(sparseseed);
+labels=unique(sparseseed(find(sparseseed>0)));
+nlabels=length(labels);
 
 % Assign the label indices
 labelindex=linspace(1,nlabels,nlabels);
 
+%Let's try this instead--I think I Need to have the indices as the seed
+%locations (the soods you were trying last night)
+label1loc=find(sparseseed==1);
+label2loc=find(sparseseed==2);
+label3loc=find(sparseseed==3);
+%label4loc=find(sparseseed==4);
+
+labelindex=[label1loc(length(label1loc)/2),label2loc(length(label2loc)/2),label3loc(length(label3loc)/2)];
+
 % Find the values (in order) of the labels
-labels=nonzeros(sparseseed)';
+%labels=nonzeros(sparseseed)';
 
 % Define the matrix M which has elements msj=1 for s=j and zero elsewhere,
 % where s and j are indices in the matrix. This matrix is used to separate
 % B with the labels, so it should be an nxn diagonal matrix with n=nseeds
-v=ones(1,nseeds);
+v=ones(1,nlabels);
 M=diag(v);
+M=sparse(M);
 
+clear v
+clear sparseseed
 
 %% Calculate BT*M (The Right Hand Side of the Equation Lu*x=BT*M). 
 %BT=-L(columnns outside the label index evaluated at each label)
 coloutsidelabel=nlabels+1:length(L);
+coloutsidelabel=sparse(coloutsidelabel);
 BT=-L(coloutsidelabel,labelindex);
+BT=sparse(BT);
 RHS=BT*M;
 
+clear BT
 %% Use Matlab's implicit system of equations solver to find the potentials matrix X
 
 X=L(coloutsidelabel,coloutsidelabel)\RHS;
@@ -95,12 +123,13 @@ P=[M;X];
 
 % Find the label to which each point should belong by taking the maximum
 % along each row (dimension 2) and finding the index for that point. 
-[maxvalue assignedlabel]=max(P,[],2);
+[maxvalue, assignedlabel]=max(P,[],2);
 
 %Combine all of these labels into a max in the dimensions of the original
 %image
-mask=reshape(assignedlabel,xdim,ydim);
-
+mask=reshape(assignedlabel, [xdim,ydim]);
+P=full(P);
+P=reshape(P, [xdim,ydim,nlabels]);
 
 end
 
