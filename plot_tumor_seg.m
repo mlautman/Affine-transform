9f9e1965fdd0b0e8d961cbc3f230cc8106f7739b
mlautman <mikeblautman@gmail.com>
2015-04-27 04:11:12 -0400

    final results

--- plot_tumor_seg.m
+++ plot_tumor_seg.m
@@ -1,14 +1,15 @@
 function plot_tumor_seg(data)
 % close all;
 % plot
+    
 figure(1)
 for i = 1:length(data.ext)-2
     subplot(3,2,i);
     imshow(...
         reshape(data.img(:,:,i), [240 240]) / ...
-        max(max(max(data.img(:,:,i)))));    
+        max(max(max(data.img(:,:,i)))));
     title(data.ext{i}(2:find(data.ext{i}=='.')-1))
-end   
+end
 
 i = i+1;
 subplot(3,2,i);
@@ -24,24 +25,24 @@ imshow(...
     max(max(max(data.seg(:,:)))));    
 title(data.ext{i}(2:find(data.ext{i}=='.')-1))
 
-% figure(2)
-% imshow(...
-%     reshape(data.img(:,:,1), [240 240]) / ...
-%     max(max(max(data.img(:,:,1)))));
-% hold on
-% [j,i] = find(data.seg == 1);
-% scatter(i,j, 'r.')
-% [j,i] = find(data.seg == 2);
-% scatter(i,j, 'g.')
-% [j,i] = find(data.seg == 3);
-% scatter(i,j, 'b.')
-% 
-% [j,i] = find(data.seed == 1);
-% scatter(i,j, 'm.')
-% [j,i] = find(data.seed == 2);
-% scatter(i,j, 'y')
-% [j,i] = find(data.seed == 3);
-% scatter(i,j, 'b')
+figure(2)
+imshow(...
+    squeeze(data.img(:,:,1)) ./ ...
+    max(max(max(data.img(:,:,1)))));
+hold on
+[j,i] = find(data.seg == 1);
+scatter(i,j, 'r.')
+[j,i] = find(data.seg == 2);
+scatter(i,j, 'g.')
+[j,i] = find(data.seg == 3);
+scatter(i,j, 'b.')
+
+[j,i] = find(data.seed == 1);
+scatter(i,j, 'm.')
+[j,i] = find(data.seed == 2);
+scatter(i,j, 'y')
+[j,i] = find(data.seed == 3);
+scatter(i,j, 'b')
 
 end
 
