514e9de87f46bd938652a3bbdea80d849aa8ddd8
sugirl06 <alundb@sas.upenn.edu>
2015-04-27 11:15:39 -0400

    Update seg_eval.m

--- seg_eval.m
+++ seg_eval.m
@@ -4,7 +4,7 @@ avedist=[];
 hausdist=[];
 dice = [];
 % %  Need to loop over number of regions in img
-for k = 1:length(unique(gtr))-1
+for k = 1:length(unique(gtr))
     set1 = find(gtr == k);
     data1 = zeros(size(gtr));
     data1(set1) = 1;
@@ -15,27 +15,27 @@ for k = 1:length(unique(gtr))-1
 
     GSplus = data1+data2;
     GSintersect = find(GSplus == 2);
-    dice =[dice; 2*length(GSintersect)/(sum(sum((data1))) + sum(sum(data2)))];
+    dice =[dice; k, 2*length(GSintersect)/(sum(sum((data1))) + sum(sum(data2)))];
        
     data1 = edge(data1);
     data2 = edge(data2);
     
 % %     Distance from set 1 to set 2
     dist12a = bwdist(data1);
-    [i,j] = find(data2==k);
+    [i,j] = find(data2==1);
     for ijk = 1:length(i)
         dist12(ijk) = dist12a(i(ijk), j(ijk));
     end
     
 % %     Distance from set 2 to set 1
     dist21a = bwdist(data2);
-    [i,j] = find(data1==k);
+    [i,j] = find(data1==1);
     for ijk = 1:length(i)
         dist21(ijk) = dist21a(i(ijk), j(ijk));
     end
     
-    avedist = [avedist;  mean([mean(dist12), mean(dist21)])];
-    hausdist = [hausdist;  mean([max(dist12), max(dist21)])];
+    avedist = [avedist; k, mean([mean(dist12), mean(dist21)])];
+    hausdist = [hausdist; k, mean([max(dist12), max(dist21)])];
     
 end
 
